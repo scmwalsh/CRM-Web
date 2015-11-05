bee94626ce2b48809528c0bbe849af6407a22b43
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-11-05 14:56:05 -0500

    we can now edit

--- crm.rb
+++ crm.rb
@@ -32,6 +32,15 @@ get "/contacts/:id" do
 	end
 end
 
+get "/contacts/:id/edit" do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+  	erb :edit_contacts
+  else
+  	raise Sinatra::NotFound
+  end
+end
+
 
 
 
