f7aaf5e6c0b703573d51259b9e6dcff1eb129b83
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-11-07 13:22:36 -0500

    revised working edit function

--- crm.rb
+++ crm.rb
@@ -32,6 +32,7 @@ get '/' do
 end
 
 get '/contacts' do
+  @conacts = Contact.all
 	erb :contacts
 end
 
@@ -40,12 +41,13 @@ get '/contacts/new' do
 end
 
 post '/contacts' do
-Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+Contact.create(:first_name => params[:first_name], :last_name => params[:last_name], :email => params[:email], :notes => params[:notes])
 redirect to ('/contacts')
 end
 
+
 get "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
   	erb :show_contact
   else
@@ -54,7 +56,7 @@ get "/contacts/:id" do
 end
 
 get "/contacts/:id/edit" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
   	erb :edit_contacts
   else
@@ -63,12 +65,13 @@ get "/contacts/:id/edit" do
 end
 
 put "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.notes = params[:notes]
+    @contact.update(:first_name => params[:first_name], :last_name => params[:last_name], :email => params[:email], :notes => params[:notes])
+    # @contact.first_name = params[:first_name]
+    # @contact.last_name = params[:last_name]
+    # @contact.email = params[:email]
+    # @contact.notes = params[:notes]
 
     redirect to("/contacts")
   else
@@ -77,9 +80,9 @@ put "/contacts/:id" do
 end
 
 delete "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
-    @contact.remove
+    @contact.destroy
     redirect to("/contacts")
   else
     raise Sinatra::NotFound
